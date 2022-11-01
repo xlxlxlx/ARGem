9bb8eedbd1dc78fa6c3b466807ab4efba33b2fc4
xlxlxlx <32310857+xlxlxlx@users.noreply.github.com>
2022-10-31 21:28:33 -0400

    Update megahit_driver.py

--- driver_assembly/megahit_driver.py
+++ driver_assembly/megahit_driver.py
@@ -10,57 +10,47 @@ import json
 
 class MegaHitDriver:
     '''
-    Class responsible for running the MEGAHIT Assembler. (Kevin)
+    Class responsible for running the MEGAHIT Assembler.
     '''
     def __init__(self, proj_path):
 
         # this is the project folder the results are outputted
         self.proj_path = proj_path
 
-        self.megahit_path = "../../../assembly_tools/MEGAHIT-1.2.9-Linux-x86_64-static/bin/"
-        self.SRA_path = f"{proj_path}/raw_sequence_data"
+        self.megahit_path = ""
+        self.SRA_path = "sra_data"
 
     def run_megahit(self):
-        sra_table = {}
-        sra_num_list = []
-        result = subprocess.getoutput(f'find {self.SRA_path}/SRR*')
-        result = result.split('\n')
-
-        # Find all SRA accession numbers in the designated directory
-        for sra_folder in result:
-            sra_folder = sra_folder.replace(f'{proj_path}/raw_sequence_data/', '')
-
-            if re.search("SRR[0-9]+$", sra_folder, re.IGNORECASE) != None:
-                sra_num_list.append(sra_folder)
-
-        # Determines if the directories for each SRA number contain the correct number of fasta files
+        sra_num_list = []      
+        
+        with open(os.path.join(self.proj_path, 'SRA_list'), "r", encoding="utf-8") as sra_list_file:
+            lines = sra_list_file.readlines()
+            sra_num_list = lines[0].split(',')[:-1]
+     
         print(sra_num_list)
-        for sra_num in sra_num_list:
-            for sra_folder in result:
-                if re.search(f"{sra_num}_1.fastq", sra_folder) != None:
-                    sra_table[sra_num] = 2
-                    break
-                else:
-                    sra_table[sra_num] = 1
 
         error_count = 0
-        for key in sra_table:
-            value = sra_table[key]
-
+        for key in sra_num_list:
+            key = key.strip()
+            file_one = f"{self.SRA_path}/{key}/{key}_1.fastq"
+            file_two = f"{self.SRA_path}/{key}/{key}_2.fastq"
+            
             # Only 1 or less fasta file is present in the directory -> throws error
-            if value == 1:
-                error_message = "Error: Only one fastq file is available for assembly."
+            if not os.path.exists(file_one) or not os.path.exists(file_two):
+                error_message = "Error: no enought fastq files available for assembly."
+                print(error_message)
+                print(file_one, file_two)
                 error_count += 1
                 self.error_log(key, error_message)
 
             # Directory correctly contains two fasta files for annotation
-            elif value == 2:
-                file_one = f"{self.SRA_path}/{key}/{key}_1.fastq"
-                file_two = f"{self.SRA_path}/{key}/{key}_2.fastq"
+            else:
                 output_dir = f"{self.proj_path}/assembly_output/{key}_assembled"
+                print(f"mkdir -p {self.proj_path}/assembly_output")
                 subprocess.run(f"mkdir -p {self.proj_path}/assembly_output", shell=True)
 
                 # call MEGAHIT to assemble the contigs
+                print(f"{self.megahit_path}/megahit -1 {file_one} -2 {file_two} -o {output_dir}")
                 subprocess.run(f"{self.megahit_path}/megahit -1 {file_one} -2 {file_two} -o {output_dir}", shell=True)
 
         # write to outcome.json
@@ -70,7 +60,7 @@ class MegaHitDriver:
             if error_count == 0:
                 data['assembly_outcome'] = 'Success'
             # case 2: some errors, some successes
-            elif error_count < len(sra_table):
+            elif error_count < len(sra_num_list):
                 data['assembly_outcome'] = 'Success*'
             # case 3: all errors
             else:
@@ -100,4 +90,3 @@ if __name__ =="__main__":
     megahitDvr.run_megahit()
 
     print(f"[Total Time Spent: {round((time.time() - ini_time), 2)} seconds]")
-
