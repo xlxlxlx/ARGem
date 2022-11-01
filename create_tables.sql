c33fd125ea3901eec7ace3955406b95405c8a485
xlxlxlx <32310857+xlxlxlx@users.noreply.github.com>
2022-10-31 20:59:15 -0400

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -6,39 +6,6 @@ CREATE TABLE `metadata_attribute` (
   `required_att` tinyint(1)
 );
 
-CREATE TABLE `project` (
-  `Project_ID` varchar(50) PRIMARY KEY,
-  `Name` varchar(255),
-  `is_public` tinyint(1),
-  `Owner_ID` bigint(20),
-  `Creator_ID` bigint(20),
-  `Creation_date` data(6),
-  `Directory` varchar(255)
-);
-
-CREATE TABLE `user_project` (
-  `Project_ID` varchar(50),
-  `User_ID` bigint(20),
-  `Type` tinyint(1),
-  PRIMARY KEY (`User_ID`, `Project_ID`)
-);
-
-CREATE TABLE `share_project` (
-  `Project_ID` varchar(50),
-  `Share_ID` varchar(50),
-  `Type` tinyint(1),
-  PRIMARY KEY (`Share_ID`, `Project_ID`)
-);
-
-CREATE TABLE `project_file` (
-  `File_ID` varchar(50) PRIMARY KEY,
-  `File_name` varchar(255),
-  `File_display_name` varchar(255),
-  `Project_ID` varchar(50),
-  `Uploaded_by` bigint(20),
-  `Uploaded_Date` datetime
-);
-
 CREATE TABLE `sample_metadata` (
   `Project_ID` varchar(50),
   `ATT_ID` bigint(20),
@@ -48,27 +15,13 @@ CREATE TABLE `sample_metadata` (
   PRIMARY KEY (`Sample_ID`, `ATT_ID`, `Project_ID`)
 );
 
-CREATE TABLE `project_info` (
+CREATE TABLE `project` (
   `Project_ID` varchar(50) PRIMARY KEY,
-  `Description` varchar(255),
-  `BioProject_link` varchar(255),
-  `Institute` varchar(255),
-  `Contact_email` varchar(255),
-  `Funding` varchar(255)
-);
-
-CREATE TABLE `papers` (
-  `Paper_ID` varchar(50) PRIMARY KEY,
-  `Title` varchar(255),
-  `DOI` varchar(255),
-  `link` varchar(255),
-  `PM_ID` varchar(100)
-);
-
-CREATE TABLE `project_papers` (
-  `Project_ID` varchar(50),
-  `Paper_ID` varchar(50),
-  PRIMARY KEY (`Project_ID`, `Paper_ID`)
+  `Name` varchar(255),
+  `is_public` tinyint(1),
+  `User_ID` bigint(20),
+  `Creation_date` data(6),
+  `Directory` varchar(255)
 );
 
 CREATE TABLE `project_tasks` (
@@ -78,46 +31,25 @@ CREATE TABLE `project_tasks` (
   `Output_File_ID` varchar(50),
   `Type` varchar(50),
   `Status` varchar(50),
-  `Timestamp` DATE NOT NULL
+  `Timestamp` datetime,
+  `MGE_DB` varchar(50),
+  `User_ID` bigint(20)
 );
 
 CREATE TABLE `SRA_record` (
-  `Project_ID` varchar(50) PRIMARY KEY,
   `SRA` varchar(50) NOT NULL,
-  `Assembly_date` DATE NOT NULL,
-  `Status` int NOT NULL
+  `Status` int(11) NOT NULL
 );
 
-CREATE TABLE `user_info` (
+CREATE TABLE `users` (
   `User_ID` bigint(20) PRIMARY KEY,
-  `User_type` int NOT NULL
+  `User_email` varchar(100)
 );
 
-ALTER TABLE `user_info` ADD FOREIGN KEY (`User_ID`) REFERENCES `project` (`Owner_ID`);
-
-ALTER TABLE `user_project` ADD FOREIGN KEY (`Project_ID`) REFERENCES `project` (`Project_ID`);
-
-ALTER TABLE `SRA_record` ADD FOREIGN KEY (`Project_ID`) REFERENCES `project` (`Project_ID`);
-
-ALTER TABLE `share_project` ADD FOREIGN KEY (`Project_ID`) REFERENCES `project` (`Project_ID`);
-
-ALTER TABLE `project_file` ADD FOREIGN KEY (`Project_ID`) REFERENCES `project` (`Project_ID`);
+ALTER TABLE `users` ADD FOREIGN KEY (`User_ID`) REFERENCES `project` (`User_ID`);
 
 ALTER TABLE `sample_metadata` ADD FOREIGN KEY (`Project_ID`) REFERENCES `project` (`Project_ID`);
 
 ALTER TABLE `sample_metadata` ADD FOREIGN KEY (`ATT_ID`) REFERENCES `metadata_attribute` (`ATT_ID`);
 
-ALTER TABLE `sample_metadata` ADD FOREIGN KEY (`File_ID`) REFERENCES `project_file` (`File_ID`);
-
-ALTER TABLE `project_info` ADD FOREIGN KEY (`Project_ID`) REFERENCES `project` (`Project_ID`);
-
-ALTER TABLE `project_papers` ADD FOREIGN KEY (`Project_ID`) REFERENCES `project` (`Project_ID`);
-
-ALTER TABLE `project_papers` ADD FOREIGN KEY (`Paper_ID`) REFERENCES `papers` (`Paper_ID`);
-
-ALTER TABLE `project_tasks` ADD FOREIGN KEY (`Project_ID`) REFERENCES `project` (`Project_ID`);
-
-ALTER TABLE `project_tasks` ADD FOREIGN KEY (`Input_File_ID`) REFERENCES `project_file` (`File_ID`);
-
-ALTER TABLE `project_tasks` ADD FOREIGN KEY (`Output_File_ID`) REFERENCES `project_file` (`File_ID`);
-
+ALTER TABLE `project` ADD FOREIGN KEY (`Project_ID`) REFERENCES `project_tasks` (`Project_ID`);
